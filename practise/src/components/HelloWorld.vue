d93b4f9e42137f6f09f2607b3ac04bc181e6d291
hwdb1111 <tying8683@gmail.com>
2025-02-13 20:38:09 +0700

    Initial commit

---
+++ practise/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="HelloWorld">
+    <div>
+      <!--    二级路由出口   -->
+      <router-view></router-view>
+    </div>
+    <div>
+      <router-link to="/frontPage">首页</router-link>
+      <router-link to="/member">会员</router-link>
+      <router-link to="/information">信息</router-link>
+      <router-link to="/mine">我的</router-link>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+}
+</script>
+
+
+<style scoped>
+a {
+  width: 100px;
+  height: 50px;
+  background-color: lightgray;
+  margin: 0 20px;
+}
+.active{
+  background-color: #42b983;
+}
+
+
+</style>
---
+++ practise/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+//  接收在 index.js 文件里面导出来的 router
+import router from './router/index'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
