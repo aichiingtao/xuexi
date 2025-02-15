3b97482beeee4ca64dd9ee79ea05aef33397e66c
hwdb1111 <tying8683@gmail.com>
2025-02-15 23:37:24 +0700

    Initial commit

---
+++ cart/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="app-container">
+  <cart-header></cart-header>
+  <caet-item></caet-item>
+  <cart-footer></cart-footer>
+
+</div>
+
+</template>
+
+<script>
+import CartHeader from "@/components/cart-header.vue";
+import CartFooter from "@/components/cart-footer.vue";
+import CaetItem from "@/views/caet-item.vue";
+
+export default {
+  components:{
+    CartHeader,
+    CaetItem,
+    CartFooter,
+  }
+}
+</script>
+
+
+
+
+<style>
+.app-container{
+  margin: 0 auto;
+  width: 800px;
+}
+
+</style>
---
+++ cart/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+  },
+  {
+    path: '/about',
+    name: 'about',
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
